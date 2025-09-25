b8231a669b8f6dc30a67e1d19aac9926a0b90db0
IUTRafaëlSimple <rafael.beaulieu@gmail.com>
2025-09-25 11:40:55 +0200

    aaaaa

--- src/main/java/com/model/Guitar.java
+++ src/main/java/com/model/Guitar.java
@@ -1,14 +1,5 @@
 package com.model;
 
-public class Guitar {
-    private GuitarSpec spec;
-    
-    public Guitar(GuitarSpec spec) {
-        this.spec = spec;
+public class Guitar extends Instrument {
     }
 
-    public GuitarSpec getSpec() {
-        return spec;
-    }
-
-}
--- src/main/java/com/model/GuitarSpec.java
+++ src/main/java/com/model/GuitarSpec.java
@@ -1,11 +1,6 @@
 package com.model;
 
 public class GuitarSpec extends InstrumentSpec {
-    private int nbrStrings;
-    private Builder builder;
-    private String model;
-    private Type type;
-    private Wood backWood, topWood;
 
     public GuitarSpec(Builder builder, String model, Type type, Wood backWood, Wood topWood, int nbrStrings) {
         super(builder, model, type, backWood, topWood);
@@ -15,26 +10,5 @@ public class GuitarSpec extends InstrumentSpec {
     public int getNbrStrings() {
         return nbrStrings;
     }
-
-    public Builder getBuilder() {
-        return builder;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public Wood getBackWood() {
-        return backWood;
-    }
-
-    public Wood getTopWood() {
-        return topWood;
-    }
-
 
 }
